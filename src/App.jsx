ba7709c58a31f17e8914107e071ec755e3c5382c
LeeBingler <binglerlee@gmail.com>
2023-06-02 17:42:35 +0200

    refactor: reunit component in order to understand react + add delete button

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,81 @@ import ToDoList from './components/ToDoList';
 function App() {
   const [newItem, setNewItem] = useState('');
   const [todos, setTodos] = useState([]);
-
+  
   function handleSubmit(event) {
     event.preventDefault();
   };
-
+  
   function handleAddBtn() {
     if (newItem.length === 0)
-      return;
-    let item = {text: newItem, completed: false}
+    return;
+    let item = {text: newItem, completed: false, key: crypto.randomUUID()};
     todos.push(item);
     setNewItem('');
     setTodos(todos);
-    console.log(todos);
   }
-
+  
+  function handleDeleteBtn(key) {
+    setTodos(currentTodos => {
+      return currentTodos.map((item, index) => {
+        if (item.key === key) {
+          currentTodos.pop(index);
+        }
+      });
+    });
+  }
+  
+  function handleCheckBox(key) {
+    setTodos(currentTodos => {
+      return currentTodos.map(item => {
+        if (item.key === key) {
+          item.completed = !item.completed;
+        }
+      });
+    });
+  }
+  
   return (
     <>
     <form 
-    className='new-item-form'
-    onSubmit={handleSubmit}>
-      <div className='form-row'>
-        <label> New Item </label>
-        <input value={newItem} 
-        onChange={event => setNewItem(event.target.value)}
-        type='text' 
-        id='item'/>
-      </div>
+      className='new-item-form'
+      onSubmit={handleSubmit}>
+        <div className='form-row'>
+          <label> New Item </label>
+          <input value={newItem} 
+          onChange={event => setNewItem(event.target.value)}
+          type='text' 
+          id='item'/>
+        </div>
       <button className='btn' onClick={handleAddBtn}> Add </button>
     </form>
-    <ToDoList list={todos} />
-    </>
-  );
-};
+    
+    <h1> To Do List </h1>
+    <ul>
+    { todos.map(item => {
 
-export default App;
+        return (
+          <li key={item.key}>
+            <label>
+              <input 
+              type='checkbox'
+              onChange={event => handleCheckBox(item.key)}
+              Checked={item.completed}/>
+              {item.text}
+            </label>
+            <button 
+            className='btn btn-danger'
+            onClick={() => handleDeleteBtn(item.key)}> 
+              Delete 
+            </button>
+          </li>
+          )
+        })
+    }
+        
+        </ul>
+        </>
+        );
+      };
+      
+      export default App;
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 
 function ToDoList(props) {
-
     let list = props.list;
 
     return (
         <>
             <h1> To Do List </h1>
             <ul>
-            {list !== [] && list.map(item => {
+            {
+            list.length !== 0 &&
+            list.map(item => {
+                console.log(item);
                 return (
                 <li>
                     <label>
-                    <input type='checkbox' checked={item.completed}/>
+                        <input 
+                        type='checkbox'
+                        onChange={props.handleCheck(item.key)}
+                        Checked={item.completed}
+                        />
                         {item.text}
                     </label>
-                    <button className='btn btn-danger'> Delete </button>
+                    <button 
+                    className='btn btn-danger'
+                    onClick={props.deleteBtnFunc}> 
+                        Delete 
+                    </button>
                 </li>
-                )
+               )
             })}
             </ul>
         </>
